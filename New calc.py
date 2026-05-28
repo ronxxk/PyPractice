d198be850f013b7d136d88cee3c267a8f93d1ceb
ronxxk <coderrblackhat@gmail.com>
2026-05-28 23:58:18 +0530

    practice code

--- New calc.py
+++ New calc.py
@@ -3,18 +3,34 @@ print(calc)
 def calculate(a, b):
     selct = input("select the operation\n Press 1 for(+) \n Press 2 for(-) \n Press 3 for(x) \n Press 4 for(/) \n")
     if selct in ["1"]:
-       z = print(a + b)
-    if selct in ["2"]:
-       z = print(a - b)    
-    if selct in ["3"]:
-       z = print(a * b)    
-    if selct in ["4"]:
-       z = print(a / b) 
-    return z
+       return a + b
+    elif selct in ["2"]:
+       return a - b    
+    elif selct in ["3"]:
+       return a * b    
+    elif selct in ["4"]:
+       return a / b 
+    else:
+        print("Invalid operation")
+        return None
 
-x = int(input("Type the 1st no\n"))
-y = int(input("Type the 2nd no\n"))
-calculate(x, y)   
 
+on = True
+last_digit = None
+while on:
+    if last_digit == None:
+       c = int(input("Type the 1st no\n"))
+    else:
+       c = last_digit
+    
+    d = int(input("Type the 2nd no\n"))
+    result = calculate(c, d)
+    if result is not None:
+        print(result)
+        last_digit = result
+    Choice = str(input("Do you wanna continue with the last result? Y/N\n")).lower()
+    if Choice != "y":
+       on = False
+     
     
     
